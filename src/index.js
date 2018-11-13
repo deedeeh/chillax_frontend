88a4ec257b3bd400695ec242741f2740efa1d567
deedeeh <dinahafez.8@gmail.com>
2018-11-13 15:33:32 +0000

    Declared variables

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const signupForm = document.querySelector('#signup-form')
+const filterForm = document.querySelector('#filter-form')
+const resultList = document.querySelector('#result-list')
+
+const resultList = document.querySelector('#result-list')
+// more info element to go into each element appended to the page
+
+usersUrl = "http://localhost:3000/api/v1/users"
+wishlistUrl = "http://localhost:3000/api/v1/user_destinations"
+destinationsUrl = "http://localhost:3000/api/v1/destinations"
+commentsUrl = "http://localhost:3000/api/v1/comments"
