0cdcb0b589718ae77aefb598c029e55b25358d5f
Ed Putans <edputans@gmail.com>
2018-11-14 10:58:58 +0000

    added price/month filter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const signupForm = document.querySelector('#signup-form')
-const filterForm = document.querySelector('#filter-form')
+const monthOrPriceFilter = document.querySelector(`#filter-form [name='month']`)
 const resultList = document.querySelector('#result-list')
 // more info element to go into each element appended to the page
 
@@ -11,7 +11,7 @@ commentsURL = "http://localhost:3000/api/v1/comments"
 
 const state = {
     currentUser: undefined,
-    destinations: []
+    destinations: [],
 }
 
 //--------------------------------------------------------------------------------------------------------
@@ -24,6 +24,7 @@ const renderDestination = destination => {
     destinationEl.innerHTML=`
       <h2>${destination.title}</h2>
       <img src="${destination.pictures[0].picture_url}">
+      <p>Recommended months: ${destination.months[0].name}</p>
       <p>Recommended budget: £${destination.price}</p>
       <div class='more-info'></div>
       <hr>
@@ -31,7 +32,7 @@ const renderDestination = destination => {
     
      destinationEl.addEventListener('click', () => {
         const moreInfoEl = document.querySelector(`div [data-id='${destination.id}'] .more-info`)
-        console.log(moreInfoEl)
+        
         moreInfoEl.innerHTML = 
         `<p>${destination.content}</p>`
 
@@ -43,21 +44,36 @@ const renderDestination = destination => {
         
      })
 
-
     resultList.appendChild(destinationEl)
-
-
-
-
+    state.renderedDestinations.push(destination)
 }
 
 
+
+//external event listeners
+
+monthOrPriceFilter.addEventListener('keyup', () => {
+    resultList.innerHTML=""
+    let filteredDestinations = state.destinations.filter(destination => {
+        return destination.months[0].name.toLowerCase().includes(monthOrPriceFilter.value.toLowerCase()) || parseInt(destination.price) <= parseInt(monthOrPriceFilter.value)
+    })
+    renderDestinations(filteredDestinations)
+})
+
+// replace title with month name (remove the month model? we have a string now instead of one_to_many relationship)
+// add months worth flying to description in innerHTML of show of each page
+
+
+
+
+
+
+//Render Destinations (run upon page load)
 const renderDestinations = destinations => 
     destinations.forEach(destination => {renderDestination(destination)})
-
-
 
 getDestinations()
     .then(destinations => {
         state.destinations = [...destinations]
+        state.renderedDestinations = [...destinations]
         renderDestinations(destinations)})
